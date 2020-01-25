e74091372712147877ed3d0e9b948d1b5e91488f
MozgOFF <ruslan-lion1999@mail.ru>
2020-01-25 10:14:24 +0600

    Knox installed

--- diplomaProject/urls.py
+++ diplomaProject/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('dp.urls')),
+    path('api/auth/', include('knox.urls')),
 ]
